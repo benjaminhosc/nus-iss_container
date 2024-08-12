bf62e04281aee00a55f7818a25100b406cf30e65
benjaminhosc <benjaminhosc@gmail.com>
2024-08-12 12:36:47 +0800

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   app.py
    	new file:   requirement.txt
    	new file:   static/images/robot_icon.jpg
    	new file:   templates/index.html

---
+++ workshop01/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    image_file = 'robot_icon.jpg'
+    text_list = [
+        "Logic will get you from A to B. Imagination will take you everywhere.",
+        "There are 10 kinds of people. Those who know binary and those who don't.",
+        "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+        "It's not that I'm so smart, it's just that I stay with problems longer.",
+        "It is pitch dark. You are likely to be eaten by a grue."
+    ]
+    selected_text = pick_random_item(text_list)
+    return render_template('index.html', display_text=selected_text, image_file=image_file)
+
+def pick_random_item(items):
+    if not items:
+        raise ValueError("The list is empty")
+    return random.choice(items)
+
+if __name__ == '__main__':
+    app.run(debug=True)
